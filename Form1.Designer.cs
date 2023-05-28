6111ff40182a4f7137e1ac89180575d4c567dc1c
BaYsTiaN23 <sebastianpreciadoaguilar@gmail.com>
2023-05-27 23:58:19 -0700

    Codigo actualizado correctamente y listo para usarse

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,151 +29,178 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
-            this.label1 = new System.Windows.Forms.Label();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.gbPuertosConexion = new System.Windows.Forms.GroupBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
-            this.groupBox1.SuspendLayout();
-            this.groupBox2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbBaudios = new System.Windows.Forms.ComboBox();
+            this.cmbPuertos = new System.Windows.Forms.ComboBox();
+            this.btnDesconectar = new System.Windows.Forms.Button();
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.gbMonitor = new System.Windows.Forms.GroupBox();
+            this.chartPulsasiones = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.tmrFrecuencia = new System.Windows.Forms.Timer(this.components);
+            this.spArduino = new System.IO.Ports.SerialPort(this.components);
+            this.gbPuertosConexion.SuspendLayout();
+            this.gbMonitor.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chartPulsasiones)).BeginInit();
             this.SuspendLayout();
             // 
-            // groupBox1
+            // gbPuertosConexion
             // 
-            this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Controls.Add(this.comboBox2);
-            this.groupBox1.Controls.Add(this.comboBox1);
-            this.groupBox1.Controls.Add(this.button2);
-            this.groupBox1.Controls.Add(this.button1);
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(1321, 162);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "PANEL DE PUERTOS SERIALES";
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(1075, 95);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(240, 46);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "DESCONECTAR";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(1075, 30);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(240, 46);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "CONECTAR";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // groupBox2
-            // 
-            this.groupBox2.Controls.Add(this.chart1);
-            this.groupBox2.Location = new System.Drawing.Point(12, 190);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(1321, 456);
-            this.groupBox2.TabIndex = 1;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "MONITOR DE PULSASIONES";
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(152, 44);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 2;
-            // 
-            // comboBox2
-            // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(152, 95);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(121, 21);
-            this.comboBox2.TabIndex = 3;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(32, 51);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "label1";
+            this.gbPuertosConexion.Controls.Add(this.label2);
+            this.gbPuertosConexion.Controls.Add(this.label1);
+            this.gbPuertosConexion.Controls.Add(this.cmbBaudios);
+            this.gbPuertosConexion.Controls.Add(this.cmbPuertos);
+            this.gbPuertosConexion.Controls.Add(this.btnDesconectar);
+            this.gbPuertosConexion.Controls.Add(this.btnConectar);
+            this.gbPuertosConexion.Location = new System.Drawing.Point(12, 12);
+            this.gbPuertosConexion.Name = "gbPuertosConexion";
+            this.gbPuertosConexion.Size = new System.Drawing.Size(1321, 162);
+            this.gbPuertosConexion.TabIndex = 0;
+            this.gbPuertosConexion.TabStop = false;
+            this.gbPuertosConexion.Text = "PANEL DE PUERTOS SERIALES";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(35, 102);
+            this.label2.Font = new System.Drawing.Font("Javanese Text", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(19, 95);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(101, 36);
             this.label2.TabIndex = 5;
-            this.label2.Text = "label2";
+            this.label2.Text = "BAUDIOS : ";
             // 
-            // chart1
+            // label1
             // 
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(7, 30);
-            this.chart1.Name = "chart1";
-            series1.ChartArea = "ChartArea1";
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(1308, 420);
-            this.chart1.TabIndex = 0;
-            this.chart1.Text = "chart1";
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Javanese Text", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(19, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 36);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "PUERTOS : ";
+            // 
+            // cmbBaudios
+            // 
+            this.cmbBaudios.Font = new System.Drawing.Font("Myanmar Text", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbBaudios.FormattingEnabled = true;
+            this.cmbBaudios.Items.AddRange(new object[] {
+            "9600",
+            "38400",
+            "57600",
+            "115200"});
+            this.cmbBaudios.Location = new System.Drawing.Point(150, 89);
+            this.cmbBaudios.Name = "cmbBaudios";
+            this.cmbBaudios.Size = new System.Drawing.Size(121, 42);
+            this.cmbBaudios.TabIndex = 3;
+            // 
+            // cmbPuertos
+            // 
+            this.cmbPuertos.Font = new System.Drawing.Font("Myanmar Text", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbPuertos.FormattingEnabled = true;
+            this.cmbPuertos.Location = new System.Drawing.Point(150, 32);
+            this.cmbPuertos.Name = "cmbPuertos";
+            this.cmbPuertos.Size = new System.Drawing.Size(121, 42);
+            this.cmbPuertos.TabIndex = 2;
+            this.cmbPuertos.DropDown += new System.EventHandler(this.PuertosDropDown);
+            // 
+            // btnDesconectar
+            // 
+            this.btnDesconectar.Font = new System.Drawing.Font("MS Reference Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDesconectar.Location = new System.Drawing.Point(1061, 95);
+            this.btnDesconectar.Name = "btnDesconectar";
+            this.btnDesconectar.Size = new System.Drawing.Size(240, 46);
+            this.btnDesconectar.TabIndex = 1;
+            this.btnDesconectar.Text = "DESCONECTAR";
+            this.btnDesconectar.UseVisualStyleBackColor = true;
+            this.btnDesconectar.Click += new System.EventHandler(this.btnDesconectar_Click);
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Font = new System.Drawing.Font("MS Reference Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConectar.Location = new System.Drawing.Point(1061, 30);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(240, 46);
+            this.btnConectar.TabIndex = 0;
+            this.btnConectar.Text = "CONECTAR";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // gbMonitor
+            // 
+            this.gbMonitor.Controls.Add(this.chartPulsasiones);
+            this.gbMonitor.Location = new System.Drawing.Point(12, 190);
+            this.gbMonitor.Name = "gbMonitor";
+            this.gbMonitor.Size = new System.Drawing.Size(1321, 594);
+            this.gbMonitor.TabIndex = 1;
+            this.gbMonitor.TabStop = false;
+            this.gbMonitor.Text = "MONITOR DE PULSASIONES";
+            // 
+            // chartPulsasiones
+            // 
+            this.chartPulsasiones.BackColor = System.Drawing.Color.Black;
+            chartArea2.AxisX.LabelStyle.ForeColor = System.Drawing.Color.White;
+            chartArea2.AxisX.LineColor = System.Drawing.Color.White;
+            chartArea2.AxisY.LabelStyle.ForeColor = System.Drawing.Color.White;
+            chartArea2.AxisY.LineColor = System.Drawing.Color.White;
+            chartArea2.BackColor = System.Drawing.Color.Black;
+            chartArea2.BackSecondaryColor = System.Drawing.Color.White;
+            chartArea2.Name = "ChartArea1";
+            chartArea2.ShadowColor = System.Drawing.Color.Silver;
+            this.chartPulsasiones.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chartPulsasiones.Legends.Add(legend2);
+            this.chartPulsasiones.Location = new System.Drawing.Point(6, 19);
+            this.chartPulsasiones.Name = "chartPulsasiones";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.Color = System.Drawing.Color.Lime;
+            series2.Legend = "Legend1";
+            series2.Name = "Analog";
+            series2.YValuesPerPoint = 4;
+            this.chartPulsasiones.Series.Add(series2);
+            this.chartPulsasiones.Size = new System.Drawing.Size(1309, 569);
+            this.chartPulsasiones.TabIndex = 4;
+            this.chartPulsasiones.Text = "chart1";
+            // 
+            // tmrFrecuencia
+            // 
+            this.tmrFrecuencia.Tick += new System.EventHandler(this.tmrFrecuencia_Tick);
             // 
             // MenuForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1345, 676);
-            this.Controls.Add(this.groupBox2);
-            this.Controls.Add(this.groupBox1);
+            this.ClientSize = new System.Drawing.Size(1345, 805);
+            this.Controls.Add(this.gbMonitor);
+            this.Controls.Add(this.gbPuertosConexion);
             this.Name = "MenuForm";
             this.Text = "ECGAD8232";
             this.Load += new System.EventHandler(this.FormLoad);
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
-            this.groupBox2.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.gbPuertosConexion.ResumeLayout(false);
+            this.gbPuertosConexion.PerformLayout();
+            this.gbMonitor.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chartPulsasiones)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox gbPuertosConexion;
+        private System.Windows.Forms.GroupBox gbMonitor;
+        private System.Windows.Forms.Button btnDesconectar;
+        private System.Windows.Forms.Button btnConectar;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox comboBox2;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
-        private System.Windows.Forms.Timer timer1;
-        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ComboBox cmbBaudios;
+        private System.Windows.Forms.ComboBox cmbPuertos;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartPulsasiones;
+        private System.Windows.Forms.Timer tmrFrecuencia;
+        private System.IO.Ports.SerialPort spArduino;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,9 @@ namespace ECGAD8232
 {
     public partial class MenuForm : Form
     {
+        delegate void serialCalback(string val);
+        double x;
+
         public MenuForm()
         {
             InitializeComponent();
@@ -19,17 +23,99 @@ namespace ECGAD8232
 
         private void FormLoad(object sender, EventArgs e)
         {
-
+            btnDesconectar.Enabled = false;
+            btnConectar.Enabled = true;
+            tmrFrecuencia.Tick += tmrFrecuencia_Tick;
+            chartPulsasiones.Series[0].IsVisibleInLegend = false;
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
 
+        private void PuertosDropDown(object sender, EventArgs e)
+        {
+            string[] portList = SerialPort.GetPortNames();
+            cmbPuertos.Items.Clear();
+            cmbPuertos.Items.AddRange(portList);
         }
 
-        private void button2_Click(object sender, EventArgs e)
-        {
+        //Metodo extra creado para mandar los datos obtenidos del arduino en la lectura analoga dirigidas al monitor serial de visual studio
 
+        private void chartsetText(string val)
+        {
+            if (this.chartPulsasiones.InvokeRequired)
+            {
+                serialCalback scb = new serialCalback(chartsetText);
+                this.BeginInvoke(scb, new object[] { val });
+            }
+            else
+            {
+                if (!String.Equals("!", val))
+                {
+                    chartPulsasiones.Series[0].Points.AddXY(x, val);
+                }
+            }
+        }
+
+        private void tmrFrecuencia_Tick(object sender, EventArgs e)
+        {
+            string incomString = spArduino.ReadLine();
+            chartsetText(incomString);
+
+            //if(String.Equals(incomString,"0"))
+            //{
+            //    serialPort.Write("0");
+            //}
+
+            if (chartPulsasiones.Series[0].Points.Count > 100) chartPulsasiones.Series[0].Points.RemoveAt(0);
+
+            chartPulsasiones.ChartAreas[0].AxisX.Minimum = chartPulsasiones.Series[0].Points[0].XValue;
+            chartPulsasiones.ChartAreas[0].AxisX.Maximum = x;
+
+            x += 1;
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            if (!spArduino.IsOpen)
+            {
+                try
+                {
+                    spArduino.PortName = cmbPuertos.Text;
+                    spArduino.BaudRate = Convert.ToInt32(cmbBaudios.Text);
+                    spArduino.Open();
+
+                    MessageBox.Show("Conexion a arduino completada");
+                    btnDesconectar.Enabled = true;
+                    btnConectar.Enabled = false;
+                    chartPulsasiones.Series[0].Points.Clear();
+                    tmrFrecuencia.Start();
+                }
+                catch (Exception error)
+                {
+                    MessageBox.Show(error.Message);
+                }
+            }
+        }
+
+        private void btnDesconectar_Click(object sender, EventArgs e)
+        {
+            if (spArduino.IsOpen)
+            {
+                try
+                {
+                    tmrFrecuencia.Stop();
+                    spArduino.Close();
+
+                    MessageBox.Show("Conexion a arduino terminada");
+                    btnDesconectar.Enabled = false;
+                    btnConectar.Enabled = true;
+                    chartPulsasiones.Series[0].Points.Clear();
+
+                }
+                catch (Exception error)
+                {
+                    MessageBox.Show(error.Message);
+                }
+            }
         }
     }
 }
